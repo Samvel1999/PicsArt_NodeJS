0f2b141bf70e3c275b00078a5b488e77ce453b0f
Samvel1999 <samvelkhachatryan1999@gmail.com>
2021-01-31 04:22:05 +0400

    Promise.race implementation

---
+++ Day_2/PromiseRace.js
@@ -0,0 +1,52 @@
+function raceImpl() {
+    let arr = arguments[0];
+
+    return new Promise((resolve, reject) => {
+        if(!Array.isArray(arr)) {
+            reject("Is not iterable.")
+        }
+
+        for(let elem of arr) {
+            elem.then((val) => {
+                resolve(val);
+            }).catch((err) => {
+                reject(err);
+            })
+        }
+    })
+}
+
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(function () {
+        let isTrue = false;
+        if(isTrue) {
+            resolve("p1 resolved");
+        }
+        else {
+            reject("p1 rejected");
+        }
+    },7000);
+});
+
+let p2 = Promise.resolve(13);
+let p3 = Promise.resolve("Success");
+
+let p4 = new Promise((resolve, reject) => {
+    setTimeout(function () {
+        let isTrue = false;
+        if(isTrue) {
+            resolve("p4 resolved");
+        }
+        else {
+            reject("p4 rejected");
+        }
+    },1000);
+});
+
+let arr = [p1, p4];
+
+Promise.race = raceImpl;
+
+Promise.race(arr)
+    .then(val => console.log(val))
+    .catch(err => console.log(err));
